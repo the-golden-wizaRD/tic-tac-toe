728ec03988084fb602404df44fd8482f0070cec5
the-golden-wizaRD <rdtiwari.official@gmail.com>
2020-04-06 18:51:01 +0530

    re-adding project

---
+++ build/precache-manifest.56515290b7459dbbc9d7687649a59e9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30041b212abedaf381215e2c0aaf0325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "022a9441ff558186ea34",
+    "url": "/static/css/main.46e31b54.chunk.css"
+  },
+  {
+    "revision": "77bc90d265ce62869905",
+    "url": "/static/js/2.2b9d78a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b9d78a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "022a9441ff558186ea34",
+    "url": "/static/js/main.635c2e8c.chunk.js"
+  },
+  {
+    "revision": "f6633bf07a047e0bb61c",
+    "url": "/static/js/runtime-main.23127ecf.js"
+  }
+]);
